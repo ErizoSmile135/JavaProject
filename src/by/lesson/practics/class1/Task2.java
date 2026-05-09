31ae9105c230d8ac533733d0978a52fd78ac9ed3
Maksim <maxyarmoluk@gmail.com>
2026-05-09 18:13:18 +0300

    Практика 1 (все задачи)

--- src/by/lesson/practics/class1/Task2.java
+++ src/by/lesson/practics/class1/Task2.java
@@ -54,14 +54,50 @@ public class Task2 {
         System.out.println("*********");
         System.out.println("Задание 3");
 
+        System.out.printf("a = ");
+        a = sc.nextInt();
+        if (a % 3 == 0) {
+            System.out.println(a + " делится на 3");
+        } else {
+            System.out.println(a + " не делится на 3");
+        }
+
         // 4. На вход дано число. Определить, делиться ли оно на 3 и на 7, если нет, тогда проверить делиться ли оно на 3, если не делиться, то проверить, делиться ли оно на 7. Вывести соответствующий текст.
         System.out.println("*********");
         System.out.println("Задание 4");
+
+        System.out.printf("a = ");
+        a = sc.nextInt();
+
+        switch ((a % 3) + (a % 7)) {
+            case 0:
+                System.out.println("делится на 3 и 7");
+            default:
+                switch (a % 3) {
+                    case 0:
+                        System.out.println("Делится на 3");
+                    default:
+                        switch (a % 7) {
+                            case 0:
+                                System.out.println("Делится на 7");
+                            default:
+                                System.out.println("не делит на 3 и 7");
+                        }
+                }
+        }
 
         // 6. На вход даны три числа. Определить максимальное из них
         System.out.println("*********");
         System.out.println("Задание 5");
 
+        System.out.printf("a = ");
+        a = sc.nextInt();
+        System.out.printf("b = ");
+        b = sc.nextInt();
+        System.out.printf("c = ");
+        c = sc.nextInt();
+
+        System.out.println("max = " + Math.max(a, Math.max(b, c)));
 
     }
 }
